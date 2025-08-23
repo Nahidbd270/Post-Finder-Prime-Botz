1e7707fc0042dc56408affd476951f6ef398eee3
Nahidbd270 <Nahidbd270@gmail.com>
2025-08-23 09:36:26 +0600

    Update info.py

--- info.py
+++ info.py
@@ -3,5 +3,5 @@ API_HASH     = "a5b1ff3f75941649bf5bc159782f0f00"
 BOT_TOKEN    = "8101519498:AAFXjg4kyJsZAdaF14fkmRwMq73QYfkliAY"
 SESSION      = "BQG4hlIAcfoe9ivMHs9P6K4oU4z4l18so9LFggqrk93rmdzyQxMzCSzclhfEZeAnR39aS6iSVUjAPWrzwPRQcnV-qZCGGZjQftcYnm3e5oKqmj0JPr9vJn2ueJfJqSvYZU0Rvxv3O3jrbrNiAHYucjrK2-BBPZEb48tfYPXoB1iUy5UczOfinBBgTljnDrxyQz7IP8hCZI7YMuEyQLM2DnQW-eac4MTgz6hzfEqOPpayOWY9myOkPgDqhL2E98QnSRQVkA6Oh9On4qDl6QInmvjI1Y8ob1PzmvU9Pud5S9pb83tFXZwnLBhND9BUzenfev4LyYAc09cfegliEDlmQvhtcMIfJQAAAAHAvfxpAA"
 DATABASE_URI = "mongodb+srv://polay60829:polay60829@cluster0.2qxpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = -1002430341209
+LOG_CHANNEL  = -1003044100338
 ADMIN        = 7528643689
