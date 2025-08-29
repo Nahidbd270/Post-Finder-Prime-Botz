d87855336d5eb1ddaa89e1198c6adf12e2d680b8
Nahidbd270 <Nahidbd270@gmail.com>
2025-08-29 09:18:32 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 28870226
 API_HASH     = "a5b1ff3f75941649bf5bc159782f0f00"
-BOT_TOKEN    = "8101519498:AAFXjg4kyJsZAdaF14fkmRwMq73QYfkliAY"
+BOT_TOKEN    = "8101519498:AAHOdrroe2GyhhcSwykDN0IpPsVrXCJ0htE"
 SESSION      = "BQG4hlIADYIkdwz1UzQa95uRjqrlMibtuZZDWhSLmmzpyYGOr8hMod4os4Y79_0qbrvfoa-wGbm9sLqkOnHh0LzVvJIvrKDQNGOBkDeTgF0kFbaW2uoWCOuiAJh25bt3g24N4nkqNF9d-yivRQrdH1EMJuOUnXqwQkIPT2w3yqTkjQRPYO3BMtqE-GcbwUuk8uGy8MK6KNmISMQreYvmFI7h0ofdZ_7XdxsIevplHef12w39WHj-SUGhIfVVEfDi1Y76RbXniOzOqaf2VANnSz85va6zWps833Lgq5Th9u17OgJcIHAq77fhEgyVhspv68HDzYqWMiMXmpldKp8x2rVG_a0xOgAAAAHAvfxpAA"
 DATABASE_URI = "mongodb+srv://polay60829:polay60829@cluster0.2qxpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1003044100338
